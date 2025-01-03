35dc173b84fefcbd024675344c715fb75e099a85
krudoppler <bkru33@gmail.com>
2025-01-02 20:01:44 -0500

    figured out the adding menu items and total cost

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 import { menuArray } from "./data.js";
 // Global Variable
-let selectedItems = []
+let selectedItems = [];
 
 // DOM Elements
 const menuContainer = document.getElementById("menu-container");
+const orderContainer = document.getElementById("order-container");
 
 // Event Listeners
 document.addEventListener("click", function (e) {
   if (e.target.dataset.pizza) {
     addItemToOrder("Pizza", 14);
   } else if (e.target.dataset.hamburger) {
-    getHamburger();
+    addItemToOrder("Hamburger", 12);
   } else if (e.target.dataset.beer) {
-    getBeer();
+    addItemToOrder("Beer", 12);
   }
 });
 
@@ -59,10 +60,36 @@ function renderMenuItems() {
 
 renderMenuItems();
 
-function addItemToOrder(){
-
+function addItemToOrder(name, price) {
+  selectedItems.push({ name, price });
+  renderOrder();
 }
 
 function renderOrder() {
+  let orderHTML = `
+      <div class="order-title-container">
+        <h2>Your Order</h2>
+      </div>
+  `;
+  let totalPrice = 0;
 
+  selectedItems.forEach((order, index) => {
+    orderHTML += `
+    <div class="order-item">
+      <div class="order-name">${order.name}</div>
+      <div class="order-price">$${order.price}</div>
+    </div>
+    `;
+    totalPrice += order.price;
+  });
+
+  orderHTML += `
+  <hr class="divider">
+    <div class="total-price">
+      <div class="text-price">Total Price:</div> 
+      <div class="order-total-price">$${totalPrice}</div>
+    </div>
+  `;
+  orderContainer.innerHTML = orderHTML;
+  console.log(totalPrice);
 }
